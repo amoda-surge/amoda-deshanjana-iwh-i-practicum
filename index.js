cc5e976d32de32e3f4ec933597a8ba1bb4684794
amoda-surge <amoda@surge.global>
2026-03-14 11:40:45 +0530

    add home pug & update route

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', async (req, res) => {
     try {
         const resp = await axios.get(customObjects, { headers });
         const data = resp.data.results;
-        //res.render('homepage', { title: 'Monsters | HubSpot CRM', data });      
+        res.render('homepage', { title: 'Monsters | HubSpot CRM', data });      
     } catch (error) {
         console.error(error);
         res.status(500).send('Error fetching data from HubSpot');
@@ -34,6 +34,9 @@ app.get('/', async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | HubSpot' });
+});
 
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
